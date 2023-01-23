4e2411c7297e55caeab44deaa806192cb0db553c
Davide Cologgi <dcologgi@e3r3p5.42roma.it>
2023-01-23 17:12:20 +0100

    Libft42

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: dcologgi <dcologgi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 11:30:07 by dcologgi          #+#    #+#             */
-/*   Updated: 2023/01/23 16:42:12 by dcologgi         ###   ########.fr       */
+/*   Updated: 2023/01/23 17:05:21 by dcologgi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,10 @@ char	*ft_substr(const char *s, unsigned int start, size_t len)
 	char	*sub;
 	size_t	i;
 
+	i = 0;
 	if (!s || start > ft_strlen(s))
 		return (ft_strdup(""));
-	i = 0;
-	sub = (char *)malloc(sizeof(char) * (len + 1));
+	sub = malloc(sizeof(char) * (len + 1));
 	if (!sub)
 		return (0);
 	ft_bzero(sub, len + 1);
